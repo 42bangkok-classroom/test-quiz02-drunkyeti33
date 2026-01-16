ebb38e03d520606c223bb909dbaf43682b961e0e
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-16 19:29:50 +0700

    a

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,1 +1,22 @@
-export function filterUserById(id) {}
+import axios from "axios"
+export async function filterUserById(id) {
+    try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const users = response.data;
+
+    const user = users.find(item => item.id === id);
+
+    if (!user) {
+      return "Invalid id";
+    }
+
+    return {
+      id: user.id ?? null,
+      name: user.name ?? null,
+      phone: user.phone ?? null,
+      address: user.address ?? null
+    };
+  } catch (error) {
+    throw error;
+  }
+}
