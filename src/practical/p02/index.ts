2f295fe74e641d6f09a517323f796327be0b483d
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-16 19:27:56 +0700

    a

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -20,34 +20,3 @@ type newUser = {
     bs: string;
   };
 };
-import axios from "axios";
-export async function addUser(newUser: newUser | null){
-  try {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    const users = response.data;
- 
-    const formattedUsers = users.map(user => ({
-      id: user.id ?? null,
-      name: user.name ?? null,
-      phone: user.phone ?? null,
-      address: user.address ?? null
-    }));
- 
-    if (newUser === null) {
-      return formattedUsers;
-    }
-
-    const lastId = users.length > 0 ? users[users.length - 1].id : 0;
-
-    const newUser = {
-      id: lastId + 1,
-      name: newUser?.name ?? null,
-      phone: newUser?.phone ?? null,
-      address: newUser?.address ?? null
-    };
-
-    return [...formattedUsers, newUser];
-  } catch (error) {
-    throw error;
-  }
-}
