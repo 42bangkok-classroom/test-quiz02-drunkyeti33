cc03801f79f78ad006c87256782c9c8b51814246
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-16 19:11:26 +0700

    a

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -5,12 +5,12 @@ export async function getPostalAddress() {
     const response = await axios.get("https://jsonplaceholder.typicode.com/users");
     const users = response.data;
 
-    return users.map(user => ({
-      id: user.id ?? null,
-      name: user.name ?? null,
-      phone: user.phone ?? null,
-      address: user.address ?? null
-    }));
+    return users.map({
+      id: users.id ?? null,
+      name: users.name ?? null,
+      phone: users.phone ?? null,
+      address: users.address ?? null
+    });
   } catch (error) {
     throw error;
   }
