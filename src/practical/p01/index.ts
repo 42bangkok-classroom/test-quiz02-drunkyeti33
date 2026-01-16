a6af040e30713ced5629b31f46b46bf3fce96836
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-16 18:57:46 +0700

    a

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,10 @@
-export function getPostalAddress() {}
+import axios from "axios";
+
+export async function getPostalAddress() {}
+ try {
+    const res = await axios.get(https://jsonplaceholder.typicode.com/users)
+    const users = res.data;
+    }
+    return users.
+
+
